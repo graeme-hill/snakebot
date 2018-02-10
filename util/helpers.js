3146c5f89a361f66f4e23c4527db552dbc372c94
Graeme Hill <graemekh@gmail.com>
2018-02-10 00:57:24 -0800

    tabs -> spaces

--- util/helpers.js
+++ util/helpers.js
@@ -24,117 +24,117 @@
  * world json object that the game sends).
  */
 function parseWorld(stringy) {
-	// These offsets show the direction you would look when searching from head
-	// part, so they are actually the opposite of the directions the arrows
-	// point.
-	const offsets = [
-		{ char: "<", x: 1, y: 0 },
-		{ char: ">", x: -1, y: 0 },
-		{ char: "^", x: 0, y: 1 },
-		{ char: "v", x: 0, y: -1 }
-	];
+    // These offsets show the direction you would look when searching from head
+    // part, so they are actually the opposite of the directions the arrows
+    // point.
+    const offsets = [
+        { char: "<", x: 1, y: 0 },
+        { char: ">", x: -1, y: 0 },
+        { char: "^", x: 0, y: 1 },
+        { char: "v", x: 0, y: -1 }
+    ];
 
-	function isNumber(char) {
-		return "0123456789".indexOf(char) >= 0;
-	}
+    function isNumber(char) {
+        return "0123456789".indexOf(char) >= 0;
+    }
 
-	function isFood(char) {
-		return char === "*";
-	}
+    function isFood(char) {
+        return char === "*";
+    }
 
-	function makePart(row, col) {
-		return {
-			object: "point",
-			x: col,
-			y: row
-		};
-	}
+    function makePart(row, col) {
+        return {
+            object: "point",
+            x: col,
+            y: row
+        };
+    }
 
-	function testSymbol(row, col, expected) {
-		if (row < 0 || col < 0 || row >= world.height || col >= world.width) {
-			return false;
-		}
-		return symbols[row][col] === expected;
-	}
+    function testSymbol(row, col, expected) {
+        if (row < 0 || col < 0 || row >= world.height || col >= world.width) {
+            return false;
+        }
+        return symbols[row][col] === expected;
+    }
 
-	function findNextPart(row, col) {
-		for (const offset of offsets) {
-			const nextRow = row + offset.y;
-			const nextCol = col + offset.x;
-			if (testSymbol(nextRow, nextCol, offset.char)) {
-				return { nextRow, nextCol };
-			}
-		}
-		return null;
-	}
+    function findNextPart(row, col) {
+        for (const offset of offsets) {
+            const nextRow = row + offset.y;
+            const nextCol = col + offset.x;
+            if (testSymbol(nextRow, nextCol, offset.char)) {
+                return { nextRow, nextCol };
+            }
+        }
+        return null;
+    }
 
-	function makeSnakeParts(row, col, parts) {
-		debugger;
-		parts = parts || [];
-		parts = parts.concat([makePart(row, col)]);
-		const next = findNextPart(row, col);
-		if (next) {
-			parts = makeSnakeParts(next.nextRow, next.nextCol, parts);
-		}
-		return parts;
-	}
+    function makeSnakeParts(row, col, parts) {
+        debugger;
+        parts = parts || [];
+        parts = parts.concat([makePart(row, col)]);
+        const next = findNextPart(row, col);
+        if (next) {
+            parts = makeSnakeParts(next.nextRow, next.nextCol, parts);
+        }
+        return parts;
+    }
 
-	function makeSnake(number, parts) {
-		return {
-			body: {
-				data: parts
-			},
-			health: 100, // TODO: do not hard code
-			length: parts.length, // TODO: consider possible bunching on first turns
-			id: number.toString(), // normally this is a guid but any string should work the same
-		};
-	}
+    function makeSnake(number, parts) {
+        return {
+            body: {
+                data: parts
+            },
+            health: 100, // TODO: do not hard code
+            length: parts.length, // TODO: consider possible bunching on first turns
+            id: number.toString(), // normally this is a guid but any string should work the same
+        };
+    }
 
-	function makeFood(row, col) {
-		return {
-			object: "point",
-			x: col,
-			y: row
-		};
-	}
+    function makeFood(row, col) {
+        return {
+            object: "point",
+            x: col,
+            y: row
+        };
+    }
 
-	const world = {
-		food: {
-			data: []
-		},
-		snakes: {
-			data: []
-		}
-	};
+    const world = {
+        food: {
+            data: []
+        },
+        snakes: {
+            data: []
+        }
+    };
 
-	const symbols = stringy.map(s => s.replace(/\s/g, "").toLowerCase());
-	world.width = symbols[0].length;
-	world.height = symbols.length;
+    const symbols = stringy.map(s => s.replace(/\s/g, "").toLowerCase());
+    world.width = symbols[0].length;
+    world.height = symbols.length;
 
-	const allRowsAreSameLength = symbols.every(row => world.width === row.length);
-	if (!allRowsAreSameLength) {
-		throw "Bad map. All rows should be the same length.";
-	}
+    const allRowsAreSameLength = symbols.every(row => world.width === row.length);
+    if (!allRowsAreSameLength) {
+        throw "Bad map. All rows should be the same length.";
+    }
 
-	for (let rowIndex = 0; rowIndex < symbols.length; rowIndex++) {
-		const row = symbols[rowIndex];
-		for (let colIndex = 0; colIndex < row.length; colIndex++) {
-			const char = row[colIndex];
-			if (isNumber(char)) {
-				const snakeNumber = parseInt(char);
-				const parts = makeSnakeParts(rowIndex, colIndex);
-				const snake = makeSnake(snakeNumber, parts);
-				world.snakes.data.push(snake);
-				if (snakeNumber === 0) {
-					world.you = snake;
-				}
-			} else if (isFood(char)) {
-				world.food.data.push(makeFood(rowIndex, colIndex));
-			}
-		}
-	}
+    for (let rowIndex = 0; rowIndex < symbols.length; rowIndex++) {
+        const row = symbols[rowIndex];
+        for (let colIndex = 0; colIndex < row.length; colIndex++) {
+            const char = row[colIndex];
+            if (isNumber(char)) {
+                const snakeNumber = parseInt(char);
+                const parts = makeSnakeParts(rowIndex, colIndex);
+                const snake = makeSnake(snakeNumber, parts);
+                world.snakes.data.push(snake);
+                if (snakeNumber === 0) {
+                    world.you = snake;
+                }
+            } else if (isFood(char)) {
+                world.food.data.push(makeFood(rowIndex, colIndex));
+            }
+        }
+    }
 
-	return world;
+    return world;
 }
 
 function isArray(x) {
@@ -153,20 +153,20 @@ function cellIndex(x, y, width) {
 }
 
 function directionBetweenNodes(fromIndex, toIndex, width) {
-	const fromCoord = deconstructCellIndex(fromIndex, width);
-	const toCoord = deconstructCellIndex(toIndex, width);
+    const fromCoord = deconstructCellIndex(fromIndex, width);
+    const toCoord = deconstructCellIndex(toIndex, width);
 
-	// Find first sensible direction assuming that they are adjacent and not diagonal.
-	if (fromCoord.x < toCoord.x) {
-		return "right";
-	}
-	if (toCoord.x < fromCoord.x) {
-		return "left";
-	}
-	if (fromCoord.y < toCoord.y) {
-		return "down";
-	}
-	return "up";
+    // Find first sensible direction assuming that they are adjacent and not diagonal.
+    if (fromCoord.x < toCoord.x) {
+        return "right";
+    }
+    if (toCoord.x < fromCoord.x) {
+        return "left";
+    }
+    if (fromCoord.y < toCoord.y) {
+        return "down";
+    }
+    return "up";
 }
 
 function outOfBounds({ x, y }, gameState) {
@@ -174,10 +174,10 @@ function outOfBounds({ x, y }, gameState) {
 }
 
 module.exports = {
-	isArray,
-	deconstructCellIndex,
-	cellIndex,
-	directionBetweenNodes,
-	outOfBounds,
-	parseWorld
+    isArray,
+    deconstructCellIndex,
+    cellIndex,
+    directionBetweenNodes,
+    outOfBounds,
+    parseWorld
 };
--- util/simulator.js
+++ util/simulator.js
@@ -1,127 +1,127 @@
 class Countdown {
-	constructor(maxMillis) {
-		this._maxMillis = maxMillis;
-		this._start = process.hrtime();
-	}
+    constructor(maxMillis) {
+        this._maxMillis = maxMillis;
+        this._start = process.hrtime();
+    }
 
-	expired() {
-		const elapsed = process.hrtime(this._start);
-		const millis = Math.round((elapsed[0]*1000) + (elapsed[1]/1000000));
-		return millis >= this._maxMillis;
-	}
+    expired() {
+        const elapsed = process.hrtime(this._start);
+        const millis = Math.round((elapsed[0]*1000) + (elapsed[1]/1000000));
+        return millis >= this._maxMillis;
+    }
 }
 
 function ranOutOfTime() {
-	// TODO ...
+    // TODO ...
 }
 
 // Get a list of snakes that disappeared 
 function updateObituaries(oldManifest, newManifest, obituaries, turn) {
-	for (const oldId in oldManifest) {
-		if (!(oldId in newManifest)) {
-			obituaries[oldId] = turn;
-		}
-	}
+    for (const oldId in oldManifest) {
+        if (!(oldId in newManifest)) {
+            obituaries[oldId] = turn;
+        }
+    }
 }
 
 function simulate(myAlgo, enemyAlgo, initialState, countdown, maxTurns) {
-	//console.log("START OF SIM");
-	//initialState._gridCache.prettyPrint();
-	//initialState.map.printVacateGrid();
+    //console.log("START OF SIM");
+    //initialState._gridCache.prettyPrint();
+    //initialState.map.printVacateGrid();
 
-	// Keep track of how far into the future we have gone
-	let turn = 0;
+    // Keep track of how far into the future we have gone
+    let turn = 0;
 
-	// Store the result state after present step in simulation
-	let currentState = initialState;
+    // Store the result state after present step in simulation
+    let currentState = initialState;
 
-	// Keep track of who dies in the future and when
-	const obituaries = {};
+    // Keep track of who dies in the future and when
+    const obituaries = {};
 
-	// For logging purposes we want to know why we stopped simulating.
-	let terminationReason = "OUT_OF_TIME";
+    // For logging purposes we want to know why we stopped simulating.
+    let terminationReason = "OUT_OF_TIME";
 
-	const simulatedMoves = []
+    const simulatedMoves = []
 
-	while (!countdown.expired()) {
-		if (turn >= maxTurns) {
-			terminationReason = "MAX_TURNS";
-			break;
-		}
+    while (!countdown.expired()) {
+        if (turn >= maxTurns) {
+            terminationReason = "MAX_TURNS";
+            break;
+        }
 
-		turn++;
+        turn++;
 
-		const myMove = {
-			id: currentState.mySnake.id,
-			direction: myAlgo.move(currentState)
-		};
-		const enemyMoves = initialState.enemies.map(enemy => {
-			return {
-				id: enemy.id,
-				direction: enemyAlgo.move(currentState.perspective(enemy))
-			};
-		});
+        const myMove = {
+            id: currentState.mySnake.id,
+            direction: myAlgo.move(currentState)
+        };
+        const enemyMoves = initialState.enemies.map(enemy => {
+            return {
+                id: enemy.id,
+                direction: enemyAlgo.move(currentState.perspective(enemy))
+            };
+        });
 
-		simulatedMoves.push(myMove.direction);
+        simulatedMoves.push(myMove.direction);
 
-		const allMoves = [myMove].concat(enemyMoves);
+        const allMoves = [myMove].concat(enemyMoves);
 
-		const newState = currentState.newStateAfterMoves(allMoves);
+        const newState = currentState.newStateAfterMoves(allMoves);
 
-		//console.log("AFTER TURN " + turn);
-		//newState._gridCache.prettyPrint();
-		//newState.map.printVacateGrid();
+        //console.log("AFTER TURN " + turn);
+        //newState._gridCache.prettyPrint();
+        //newState.map.printVacateGrid();
 
-		updateObituaries(currentState.snakes, newState.snakes, obituaries, turn);
+        updateObituaries(currentState.snakes, newState.snakes, obituaries, turn);
 
-		currentState = newState;
+        currentState = newState;
 
-		if (currentState.isLoss()) {
-			debugger;
-			terminationReason = "LOSS";
-			break;
-		}
+        if (currentState.isLoss()) {
+            debugger;
+            terminationReason = "LOSS";
+            break;
+        }
 
-		if (currentState.isWin()) {
-			terminationReason = "WIN";
-			break;
-		}
-	}
+        if (currentState.isWin()) {
+            terminationReason = "WIN";
+            break;
+        }
+    }
 
-	const result = {
-		turnsSimulated: turn,
-		endState: currentState,
-		obituaries,
-		algorithm: myAlgo,
-		terminationReason,
-		moves: simulatedMoves
-	};
+    const result = {
+        turnsSimulated: turn,
+        endState: currentState,
+        obituaries,
+        algorithm: myAlgo,
+        terminationReason,
+        moves: simulatedMoves
+    };
 
-	//console.log("sim " + myAlgo.meta.name + "/" + enemyAlgo.meta.name + " " + turn + " turns starting w/ " + result.firstMove + " ending with " + terminationReason);
-	//console.log(simulatedMoves);
+    //console.log("sim " + myAlgo.meta.name + "/" + enemyAlgo.meta.name + " " + turn + " turns starting w/ " + result.firstMove + " ending with " + terminationReason);
+    //console.log(simulatedMoves);
 
-	return result;
+    return result;
 }
 
 function simulateFutures(initialState, maxMillis, maxTurns, algorithms) {
-	const algorithmPairs = algorithms.map(a1 => algorithms.map(a2 => {
-		return {
-			myAlgorithm: a1,
-			enemyAlgorithm: a2
-		};
-	})).reduce((l, r) => {
-		return l.concat(r);
-	}, []);
+    const algorithmPairs = algorithms.map(a1 => algorithms.map(a2 => {
+        return {
+            myAlgorithm: a1,
+            enemyAlgorithm: a2
+        };
+    })).reduce((l, r) => {
+        return l.concat(r);
+    }, []);
 
-	// Equally divide simulation time between all the timelines.
-	const timePerPair = maxMillis / algorithmPairs;
+    // Equally divide simulation time between all the timelines.
+    const timePerPair = maxMillis / algorithmPairs;
 
-	const futures = algorithmPairs.map(p =>
-		simulate(p.myAlgorithm, p.enemyAlgorithm, initialState, new Countdown(timePerPair), maxTurns))
+    const futures = algorithmPairs.map(p =>
+        simulate(p.myAlgorithm, p.enemyAlgorithm, initialState, new Countdown(timePerPair), maxTurns))
 
-	return futures;
+    return futures;
 }
 
 module.exports = {
-	simulateFutures
+    simulateFutures
 };
