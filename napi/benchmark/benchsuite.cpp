506bf8a5de8c31ca0d66bcf33d87ad5c572eb597
Graeme Hill <graemekh@gmail.com>
2018-02-16 20:34:46 -0800

    Update benchmarks

--- napi/benchmark/benchsuite.cpp
+++ napi/benchmark/benchsuite.cpp
@@ -4,7 +4,7 @@
 #include "../algorithms/sim.hpp"
 #include "../timing.hpp"
 
-void simulatorOnBusyGrid()
+void simulatorOnBusyGrid1()
 {
     GameState state(parseWorld({
         "_ _ _ v _ _ _ _ _ _ _ _ _ _ _ _ _ > > v",
@@ -31,7 +31,40 @@ void simulatorOnBusyGrid()
 
     Sim sim;
 
-    benchmark("sim - one turn on a big grid", [&sim, &state]()
+    benchmark("sim - one turn on a big grid w/ a lot of food", [&sim, &state]()
+    {
+        sim.move(state);
+    });
+}
+
+void simulatorOnBusyGrid2()
+{
+    GameState state(parseWorld({
+        "_ _ _ v _ _ _ _ _ _ _ _ _ _ _ _ _ > > v",
+        "_ _ 8 v _ 7 < < < < _ _ _ _ _ _ _ _ 9 v",
+        "_ _ ^ < _ _ _ _ _ _ * _ _ _ _ _ _ _ ^ <",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ v _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ v _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ 2 _ _ _ _ _",
+        "_ _ _ _ > > 0 _ _ _ _ _ _ _ _ _ _ _ _ _",
+        "_ _ * _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ > v _ _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ * _ _ _ _ > 4 _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ _ > > 5 _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 3 < < _",
+        "_ _ _ > > 1 _ _ _ _ _ _ _ _ _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ *",
+        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ v _ _ _ _ _ _ _ _ _",
+        "_ _ _ _ _ _ _ _ _ _ v _ _ _ _ _ _ _ _ _",
+        "_ _ * _ _ _ _ _ _ _ > 6 _ _ _ _ _ _ _ _"
+    }));
+
+    Sim sim;
+
+    benchmark("sim - one turn on a big grid w/ a couple foods", [&sim, &state]()
     {
         sim.move(state);
     });
@@ -39,5 +72,44 @@ void simulatorOnBusyGrid()
 
 void BenchSuite::run()
 {
-    simulatorOnBusyGrid();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
+    simulatorOnBusyGrid1();
+    simulatorOnBusyGrid2();
 }
