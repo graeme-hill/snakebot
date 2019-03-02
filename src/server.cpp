4806d4b4771051188fbfe07426877eedf70eebef
Graeme Hill <graeme@sendwithus.com>
2019-03-01 16:25:17 -0800

    fix ping maybe

--- src/server.cpp
+++ src/server.cpp
@@ -15,14 +15,16 @@
 #include <utility>
 #include <vector>
 
-namespace http {
-namespace server {
+namespace http
+{
+namespace server
+{
 
 connection::connection(boost::asio::ip::tcp::socket socket,
-    connection_manager& manager, request_handler& handler)
-  : socket_(std::move(socket)),
-    connection_manager_(manager),
-    request_handler_(handler)
+                       connection_manager &manager, request_handler &handler)
+    : socket_(std::move(socket)),
+      connection_manager_(manager),
+      request_handler_(handler)
 {
 }
 
@@ -40,61 +42,57 @@ void connection::do_read()
 {
   auto self(shared_from_this());
   socket_.async_read_some(boost::asio::buffer(buffer_),
-      [this, self](boost::system::error_code ec, std::size_t bytes_transferred)
-      {
-        if (!ec)
-        {
-          request_parser::result_type result;
-          std::tie(result, std::ignore) = request_parser_.parse(
-              request_, buffer_.data(), buffer_.data() + bytes_transferred);
+                          [this, self](boost::system::error_code ec, std::size_t bytes_transferred) {
+                            if (!ec)
+                            {
+                              request_parser::result_type result;
+                              std::tie(result, std::ignore) = request_parser_.parse(
+                                  request_, buffer_.data(), buffer_.data() + bytes_transferred);
 
-          if (result == request_parser::good)
-          {
-            request_handler_.handle_request(request_, reply_);
-            do_write();
-          }
-          else if (result == request_parser::bad)
-          {
-            reply_ = reply::stock_reply(reply::bad_request);
-            do_write();
-          }
-          else
-          {
-            do_read();
-          }
-        }
-        else if (ec != boost::asio::error::operation_aborted)
-        {
-          connection_manager_.stop(shared_from_this());
-        }
-      });
+                              if (result == request_parser::good)
+                              {
+                                request_handler_.handle_request(request_, reply_);
+                                do_write();
+                              }
+                              else if (result == request_parser::bad)
+                              {
+                                reply_ = reply::stock_reply(reply::bad_request);
+                                do_write();
+                              }
+                              else
+                              {
+                                do_read();
+                              }
+                            }
+                            else if (ec != boost::asio::error::operation_aborted)
+                            {
+                              connection_manager_.stop(shared_from_this());
+                            }
+                          });
 }
 
 void connection::do_write()
 {
   auto self(shared_from_this());
   boost::asio::async_write(socket_, reply_.to_buffers(),
-      [this, self](boost::system::error_code ec, std::size_t)
-      {
-        if (!ec)
-        {
-          // Initiate graceful connection closure.
-          boost::system::error_code ignored_ec;
-          socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both,
-            ignored_ec);
-        }
+                           [this, self](boost::system::error_code ec, std::size_t) {
+                             if (!ec)
+                             {
+                               // Initiate graceful connection closure.
+                               boost::system::error_code ignored_ec;
+                               socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both,
+                                                ignored_ec);
+                             }
 
-        if (ec != boost::asio::error::operation_aborted)
-        {
-          connection_manager_.stop(shared_from_this());
-        }
-      });
+                             if (ec != boost::asio::error::operation_aborted)
+                             {
+                               connection_manager_.stop(shared_from_this());
+                             }
+                           });
 }
 
 } // namespace server
 } // namespace http
-
-
 
 //
 // connection_manager.cpp
@@ -106,8 +104,10 @@ void connection::do_write()
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-namespace http {
-namespace server {
+namespace http
+{
+namespace server
+{
 
 connection_manager::connection_manager()
 {
@@ -127,15 +127,13 @@ void connection_manager::stop(connection_ptr c)
 
 void connection_manager::stop_all()
 {
-  for (auto c: connections_)
+  for (auto c : connections_)
     c->stop();
   connections_.clear();
 }
 
 } // namespace server
 } // namespace http
-
-
 
 //
 // mime_types.cpp
@@ -147,26 +145,28 @@ void connection_manager::stop_all()
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-namespace http {
-namespace server {
-namespace mime_types {
+namespace http
+{
+namespace server
+{
+namespace mime_types
+{
 
 struct mapping
 {
-  const char* extension;
-  const char* mime_type;
+  const char *extension;
+  const char *mime_type;
 } mappings[] =
-{
-  { "gif", "image/gif" },
-  { "htm", "text/html" },
-  { "html", "text/html" },
-  { "jpg", "image/jpeg" },
-  { "png", "image/png" }
-};
+    {
+        {"gif", "image/gif"},
+        {"htm", "text/html"},
+        {"html", "text/html"},
+        {"jpg", "image/jpeg"},
+        {"png", "image/png"}};
 
-std::string extension_to_type(const std::string& extension)
+std::string extension_to_type(const std::string &extension)
 {
-  for (mapping m: mappings)
+  for (mapping m : mappings)
   {
     if (m.extension == extension)
     {
@@ -181,8 +181,6 @@ std::string extension_to_type(const std::string& extension)
 } // namespace server
 } // namespace http
 
-
-
 //
 // reply.cpp
 // ~~~~~~~~~
@@ -195,43 +193,46 @@ std::string extension_to_type(const std::string& extension)
 
 #include <string>
 
-namespace http {
-namespace server {
+namespace http
+{
+namespace server
+{
 
-namespace status_strings {
+namespace status_strings
+{
 
 const std::string ok =
-  "HTTP/1.0 200 OK\r\n";
+    "HTTP/1.0 200 OK\r\n";
 const std::string created =
-  "HTTP/1.0 201 Created\r\n";
+    "HTTP/1.0 201 Created\r\n";
 const std::string accepted =
-  "HTTP/1.0 202 Accepted\r\n";
+    "HTTP/1.0 202 Accepted\r\n";
 const std::string no_content =
-  "HTTP/1.0 204 No Content\r\n";
+    "HTTP/1.0 204 No Content\r\n";
 const std::string multiple_choices =
-  "HTTP/1.0 300 Multiple Choices\r\n";
+    "HTTP/1.0 300 Multiple Choices\r\n";
 const std::string moved_permanently =
-  "HTTP/1.0 301 Moved Permanently\r\n";
+    "HTTP/1.0 301 Moved Permanently\r\n";
 const std::string moved_temporarily =
-  "HTTP/1.0 302 Moved Temporarily\r\n";
+    "HTTP/1.0 302 Moved Temporarily\r\n";
 const std::string not_modified =
-  "HTTP/1.0 304 Not Modified\r\n";
+    "HTTP/1.0 304 Not Modified\r\n";
 const std::string bad_request =
-  "HTTP/1.0 400 Bad Request\r\n";
+    "HTTP/1.0 400 Bad Request\r\n";
 const std::string unauthorized =
-  "HTTP/1.0 401 Unauthorized\r\n";
+    "HTTP/1.0 401 Unauthorized\r\n";
 const std::string forbidden =
-  "HTTP/1.0 403 Forbidden\r\n";
+    "HTTP/1.0 403 Forbidden\r\n";
 const std::string not_found =
-  "HTTP/1.0 404 Not Found\r\n";
+    "HTTP/1.0 404 Not Found\r\n";
 const std::string internal_server_error =
-  "HTTP/1.0 500 Internal Server Error\r\n";
+    "HTTP/1.0 500 Internal Server Error\r\n";
 const std::string not_implemented =
-  "HTTP/1.0 501 Not Implemented\r\n";
+    "HTTP/1.0 501 Not Implemented\r\n";
 const std::string bad_gateway =
-  "HTTP/1.0 502 Bad Gateway\r\n";
+    "HTTP/1.0 502 Bad Gateway\r\n";
 const std::string service_unavailable =
-  "HTTP/1.0 503 Service Unavailable\r\n";
+    "HTTP/1.0 503 Service Unavailable\r\n";
 
 boost::asio::const_buffer to_buffer(reply::status_type status)
 {
@@ -276,10 +277,11 @@ boost::asio::const_buffer to_buffer(reply::status_type status)
 
 } // namespace status_strings
 
-namespace misc_strings {
+namespace misc_strings
+{
 
-const char name_value_separator[] = { ':', ' ' };
-const char crlf[] = { '\r', '\n' };
+const char name_value_separator[] = {':', ' '};
+const char crlf[] = {'\r', '\n'};
 
 } // namespace misc_strings
 
@@ -289,7 +291,7 @@ std::vector<boost::asio::const_buffer> reply::to_buffers()
   buffers.push_back(status_strings::to_buffer(status));
   for (std::size_t i = 0; i < headers.size(); ++i)
   {
-    header& h = headers[i];
+    header &h = headers[i];
     buffers.push_back(boost::asio::buffer(h.name));
     buffers.push_back(boost::asio::buffer(misc_strings::name_value_separator));
     buffers.push_back(boost::asio::buffer(h.value));
@@ -300,84 +302,85 @@ std::vector<boost::asio::const_buffer> reply::to_buffers()
   return buffers;
 }
 
-namespace stock_replies {
+namespace stock_replies
+{
 
 const char ok[] = "";
 const char created[] =
-  "<html>"
-  "<head><title>Created</title></head>"
-  "<body><h1>201 Created</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Created</title></head>"
+    "<body><h1>201 Created</h1></body>"
+    "</html>";
 const char accepted[] =
-  "<html>"
-  "<head><title>Accepted</title></head>"
-  "<body><h1>202 Accepted</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Accepted</title></head>"
+    "<body><h1>202 Accepted</h1></body>"
+    "</html>";
 const char no_content[] =
-  "<html>"
-  "<head><title>No Content</title></head>"
-  "<body><h1>204 Content</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>No Content</title></head>"
+    "<body><h1>204 Content</h1></body>"
+    "</html>";
 const char multiple_choices[] =
-  "<html>"
-  "<head><title>Multiple Choices</title></head>"
-  "<body><h1>300 Multiple Choices</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Multiple Choices</title></head>"
+    "<body><h1>300 Multiple Choices</h1></body>"
+    "</html>";
 const char moved_permanently[] =
-  "<html>"
-  "<head><title>Moved Permanently</title></head>"
-  "<body><h1>301 Moved Permanently</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Moved Permanently</title></head>"
+    "<body><h1>301 Moved Permanently</h1></body>"
+    "</html>";
 const char moved_temporarily[] =
-  "<html>"
-  "<head><title>Moved Temporarily</title></head>"
-  "<body><h1>302 Moved Temporarily</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Moved Temporarily</title></head>"
+    "<body><h1>302 Moved Temporarily</h1></body>"
+    "</html>";
 const char not_modified[] =
-  "<html>"
-  "<head><title>Not Modified</title></head>"
-  "<body><h1>304 Not Modified</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Not Modified</title></head>"
+    "<body><h1>304 Not Modified</h1></body>"
+    "</html>";
 const char bad_request[] =
-  "<html>"
-  "<head><title>Bad Request</title></head>"
-  "<body><h1>400 Bad Request</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Bad Request</title></head>"
+    "<body><h1>400 Bad Request</h1></body>"
+    "</html>";
 const char unauthorized[] =
-  "<html>"
-  "<head><title>Unauthorized</title></head>"
-  "<body><h1>401 Unauthorized</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Unauthorized</title></head>"
+    "<body><h1>401 Unauthorized</h1></body>"
+    "</html>";
 const char forbidden[] =
-  "<html>"
-  "<head><title>Forbidden</title></head>"
-  "<body><h1>403 Forbidden</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Forbidden</title></head>"
+    "<body><h1>403 Forbidden</h1></body>"
+    "</html>";
 const char not_found[] =
-  "<html>"
-  "<head><title>Not Found</title></head>"
-  "<body><h1>404 Not Found</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Not Found</title></head>"
+    "<body><h1>404 Not Found</h1></body>"
+    "</html>";
 const char internal_server_error[] =
-  "<html>"
-  "<head><title>Internal Server Error</title></head>"
-  "<body><h1>500 Internal Server Error</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Internal Server Error</title></head>"
+    "<body><h1>500 Internal Server Error</h1></body>"
+    "</html>";
 const char not_implemented[] =
-  "<html>"
-  "<head><title>Not Implemented</title></head>"
-  "<body><h1>501 Not Implemented</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Not Implemented</title></head>"
+    "<body><h1>501 Not Implemented</h1></body>"
+    "</html>";
 const char bad_gateway[] =
-  "<html>"
-  "<head><title>Bad Gateway</title></head>"
-  "<body><h1>502 Bad Gateway</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Bad Gateway</title></head>"
+    "<body><h1>502 Bad Gateway</h1></body>"
+    "</html>";
 const char service_unavailable[] =
-  "<html>"
-  "<head><title>Service Unavailable</title></head>"
-  "<body><h1>503 Service Unavailable</h1></body>"
-  "</html>";
+    "<html>"
+    "<head><title>Service Unavailable</title></head>"
+    "<body><h1>503 Service Unavailable</h1></body>"
+    "</html>";
 
 std::string to_string(reply::status_type status)
 {
@@ -438,8 +441,6 @@ reply reply::stock_reply(reply::status_type status)
 } // namespace server
 } // namespace http
 
-
-
 //
 // request_handler.cpp
 // ~~~~~~~~~~~~~~~~~~~
@@ -454,38 +455,40 @@ reply reply::stock_reply(reply::status_type status)
 #include <sstream>
 #include <string>
 
-namespace http {
-namespace server {
+namespace http
+{
+namespace server
+{
 
-request_handler::request_handler(const std::string& doc_root)
-  : doc_root_(doc_root)
+request_handler::request_handler(const std::string &doc_root)
+    : doc_root_(doc_root)
 {
 }
 
-void request_handler::handle_request(const request& req, reply& rep)
+void request_handler::handle_request(const request &req, reply &rep)
 {
-    if (req.uri == "/move")
-    {
-        rep.content = Dispatcher::move(req.body);
-    }
-    else if (req.uri == "/start")
-    {
-        rep.content = Dispatcher::start(req.body);
-    }
-    else
-    {
-        rep.content = "{ error: \"wat\" }";
-    }
+  if (req.uri == "/move")
+  {
+    rep.content = Dispatcher::move(req.body);
+  }
+  else if (req.uri == "/start")
+  {
+    rep.content = Dispatcher::start(req.body);
+  }
+  else
+  {
+    rep.content = "{ \"message\": \"hi\" }";
+  }
 
-    rep.status = reply::ok;
-    rep.headers.resize(2);
-    rep.headers[0].name = "Content-Length";
-    rep.headers[0].value = std::to_string(rep.content.size());
-    rep.headers[1].name = "Content-Type";
-    rep.headers[1].value = "application/json";
+  rep.status = reply::ok;
+  rep.headers.resize(2);
+  rep.headers[0].name = "Content-Length";
+  rep.headers[0].value = std::to_string(rep.content.size());
+  rep.headers[1].name = "Content-Type";
+  rep.headers[1].value = "application/json";
 }
 
-bool request_handler::url_decode(const std::string& in, std::string& out)
+bool request_handler::url_decode(const std::string &in, std::string &out)
 {
   out.clear();
   out.reserve(in.size());
@@ -527,8 +530,6 @@ bool request_handler::url_decode(const std::string& in, std::string& out)
 } // namespace server
 } // namespace http
 
-
-
 //
 // request_parser.cpp
 // ~~~~~~~~~~~~~~~~~~
@@ -539,11 +540,13 @@ bool request_handler::url_decode(const std::string& in, std::string& out)
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-namespace http {
-namespace server {
+namespace http
+{
+namespace server
+{
 
 request_parser::request_parser()
-  : state_(method_start), braceStack_(0)
+    : state_(method_start), braceStack_(0)
 {
 }
 
@@ -552,7 +555,7 @@ void request_parser::reset()
   state_ = method_start;
 }
 
-request_parser::result_type request_parser::consume(request& req, char input)
+request_parser::result_type request_parser::consume(request &req, char input)
 {
   switch (state_)
   {
@@ -806,14 +809,14 @@ request_parser::result_type request_parser::consume(request& req, char input)
   case expecting_newline_3:
     if (input == '\n')
     {
-        state_ = expecting_body;
-        return req.method == "GET" || req.method == "get"
-          ? good
-          : indeterminate;
+      state_ = expecting_body;
+      return req.method == "GET" || req.method == "get"
+                 ? good
+                 : indeterminate;
     }
     else
     {
-        return bad;
+      return bad;
     }
   case expecting_body:
     if (input == '{')
@@ -843,10 +846,25 @@ bool request_parser::is_tspecial(int c)
 {
   switch (c)
   {
-  case '(': case ')': case '<': case '>': case '@':
-  case ',': case ';': case ':': case '\\': case '"':
-  case '/': case '[': case ']': case '?': case '=':
-  case '{': case '}': case ' ': case '\t':
+  case '(':
+  case ')':
+  case '<':
+  case '>':
+  case '@':
+  case ',':
+  case ';':
+  case ':':
+  case '\\':
+  case '"':
+  case '/':
+  case '[':
+  case ']':
+  case '?':
+  case '=':
+  case '{':
+  case '}':
+  case ' ':
+  case '\t':
     return true;
   default:
     return false;
@@ -861,8 +879,6 @@ bool request_parser::is_digit(int c)
 } // namespace server
 } // namespace http
 
-
-
 //
 // server.cpp
 // ~~~~~~~~~~
@@ -876,17 +892,19 @@ bool request_parser::is_digit(int c)
 #include <signal.h>
 #include <utility>
 
-namespace http {
-namespace server {
+namespace http
+{
+namespace server
+{
 
-server::server(const std::string& address, const std::string& port,
-    const std::string& doc_root)
-  : io_service_(),
-    signals_(io_service_),
-    acceptor_(io_service_),
-    connection_manager_(),
-    socket_(io_service_),
-    request_handler_(doc_root)
+server::server(const std::string &address, const std::string &port,
+               const std::string &doc_root)
+    : io_service_(),
+      signals_(io_service_),
+      acceptor_(io_service_),
+      connection_manager_(),
+      socket_(io_service_),
+      request_handler_(doc_root)
 {
   // Register to handle the signals that indicate when the server should exit.
   // It is safe to register for the same signal multiple times in a program,
@@ -922,30 +940,28 @@ void server::run()
 void server::do_accept()
 {
   acceptor_.async_accept(socket_,
-      [this](boost::system::error_code ec)
-      {
-        // Check whether the server was stopped by a signal before this
-        // completion handler had a chance to run.
-        if (!acceptor_.is_open())
-        {
-          return;
-        }
+                         [this](boost::system::error_code ec) {
+                           // Check whether the server was stopped by a signal before this
+                           // completion handler had a chance to run.
+                           if (!acceptor_.is_open())
+                           {
+                             return;
+                           }
 
-        if (!ec)
-        {
-          connection_manager_.start(std::make_shared<connection>(
-              std::move(socket_), connection_manager_, request_handler_));
-        }
+                           if (!ec)
+                           {
+                             connection_manager_.start(std::make_shared<connection>(
+                                 std::move(socket_), connection_manager_, request_handler_));
+                           }
 
-        do_accept();
-      });
+                           do_accept();
+                         });
 }
 
 void server::do_await_stop()
 {
   signals_.async_wait(
-      [this](boost::system::error_code /*ec*/, int /*signo*/)
-      {
+      [this](boost::system::error_code /*ec*/, int /*signo*/) {
         // The server is stopped by cancelling all outstanding asynchronous
         // operations. Once all operations have finished the io_service::run()
         // call will exit.
